de14e4bf78473174a9523634cbcaa36c9474d89b
eduardo reyes <reyes.eduardo21@att.net>
2021-04-02 17:12:57 -0500

    added entire palindrome attempt

---
+++ Challenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunPaladrome();
+        }
+
+        private void RunPaladrome()
+        {
+            GetInput();
+            CheckInput();
+
+        }
+
+        private void GetInput()
+        {
+            var input = "null";
+            Console.WriteLine("Enter a word or phrase to check if it is a palidrome: ");
+            input = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("A word or phrase was not entered, please enter a word or phrase: ");
+                input = Console.ReadLine();
+            }
+    }
+
+        private void CheckInput()
+        {
+
+        }
+
+        private void WriteToConsole()
+        {
+
+        }
+    }
+}
+
+//18006814466
+//2104764533
+mortgagesercicy
